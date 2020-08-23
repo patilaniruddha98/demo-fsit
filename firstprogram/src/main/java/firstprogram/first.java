dbf73f49883c2edf68eb095c23bf8492416fd528
Aniruddha patil <patilaniruddha98@gmail.com>
2020-08-23 11:37:29 +0530

    commit and push

---
+++ firstprogram/src/main/java/firstprogram/first.java
@@ -0,0 +1,14 @@
+package firstprogram;
+
+public class first {
+
+	public first() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
